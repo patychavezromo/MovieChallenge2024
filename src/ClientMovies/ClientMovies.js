9e00cdfa0f36552a5f576be0dd34498c9438236e
Paty Chavez <patricia.araceli.chavez.romo@gmail.com>
2024-02-20 16:50:11 -0600

    se agrego el cliente para la api tmdb con fecth

---
+++ src/ClientMovies/ClientMovies.js
@@ -0,0 +1,13 @@
+
+async function getMovies(){
+    const url='https://api.themoviedb.org/3/discover/movie';
+    const apiKey='34713abba31235292ef755be25fd8bd7';
+    
+    console.log('Realizando llamada a la API de películas desde getMovies()');
+    const response= await fetch(url+'?api_key='+apiKey);
+    const responseJson= await response.json();
+    console.log('Realizando llamada a la API de películas desde getMovies()',responseJson);
+    return responseJson;
+}
+
+export default getMovies;
